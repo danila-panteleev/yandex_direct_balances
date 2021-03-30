91fe3c74b1350b3974a158c57806db01dd6da75c
virtualaizer <pont131995@gmail.com>
2021-03-30 23:16:39 +0500

    add logger

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 from yandex_direct_balances.source import (yandex_direct_balances,
                                            google_sheets_handler)
 import time
+import logging
+
+
+logging.basicConfig(filename='log', filemode='a', level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 
 if __name__ == '__main__':
